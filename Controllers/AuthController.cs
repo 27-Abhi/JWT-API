8bb2902d042acf51ef2f227cfd94a13bfbb617a6
27-abhi <abhinavkuppasad27@gmail.com>
2023-08-26 13:28:07 +0530

    returns jwt token

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace JWT_API.Controllers
@@ -9,6 +12,14 @@ namespace JWT_API.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        private IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        
 
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(UserDTO request)
@@ -30,9 +41,31 @@ namespace JWT_API.Controllers
                 return BadRequest("User not found ");
             }
 
-            return Ok("TOken donee");
+            if(!VerifyPasswordHash(request.Password,user.PassworHash,user.PasswordSalt))
+            {
+                return BadRequest("Wrong password");
+            }
+            string token = CreateToken(user);
+            return Ok(token);
         }
 
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred);
+           var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
             using(var hmac = new HMACSHA512())
